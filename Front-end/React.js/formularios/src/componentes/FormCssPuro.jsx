b0259d47de9fa3f754f06ed9617624b336780f7a
IsaiasC1999 <100697426+IsaiasC1999@users.noreply.github.com>
2023-08-01 12:01:20 -0300

    cambios-form

--- Front-end/React.js/formularios/src/componentes/FormCssPuro.jsx
+++ Front-end/React.js/formularios/src/componentes/FormCssPuro.jsx
@@ -1,31 +1,37 @@
 import React from 'react'
-
+import '../estilos/estilos-form.css'
 const FormCssPuro = () => {
   return (
-    <form>
-      <div className="div-group-form">
-            <label>Nombre</label>
-            <input type="text"/>
-      </div>
-      <div className="div-group-form">
+
+    <div className='conteiner-form'>
+        <h3>Formulario</h3>
+      <form className='form'>
+        <div className="div-group-form">
+          <label>Nombre</label>
+          <input className='input-text' type="text" placeholder='Ingrese su nombre' />
+        </div>
+        <div className="div-group-form">
           <label>Contraseña</label>
-          <input type="password" />
-      </div>
-      <div className="div-group-form">
-            <select name="" id="">
-                <option value="1">Opcion 1</option>
-                <option value="2">Opcion 1</option>
-                <option value="3">Opcion 1</option>
-                <option value="4">Opcion 1</option>
-            </select>
-      </div>
-      <div className="div-group-form">
+          <input className='input-text' type="password" />
+        </div>
+        <div className="div-group-form">
+          <label>Seleccion Pais</label>
+          <select name="" id="">
+            <option value="1">Opcion 1</option>
+            <option value="2">Opcion 2</option>
+            <option value="3">Opcion 3</option>
+            <option value="4">Opcion 4</option>
+          </select>
+        </div>
+        {/* <div className="div-group-form">
+            <label>Tipo de tarjeta</label>
+            <input type="radio" name="Debito" id="" />
+            <input type="radio" name="Credito" id="" />
+        </div> */}
+        <button className='btn-enviar'>Enviar</button>
+      </form>
+    </div>
 
-      </div>
-      <div className="div-group-form">
-
-      </div>
-    </form>
   )
 }
 
