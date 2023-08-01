3e474dee2811639d04b70fbdcd9aaf0e8856762a
IsaiasC1999 <100697426+IsaiasC1999@users.noreply.github.com>
2023-08-01 13:02:55 -0300

    sdfsdf

--- Front-end/React.js/formularios/src/componentes/FormCssPuro.jsx
+++ Front-end/React.js/formularios/src/componentes/FormCssPuro.jsx
@@ -7,15 +7,15 @@ const FormCssPuro = () => {
         <h3>Formulario</h3>
       <form className='form'>
         <div className="div-group-form">
-          <label>Nombre</label>
+          <label className='label-form'>Nombre</label>
           <input className='input-text' type="text" placeholder='Ingrese su nombre' />
         </div>
         <div className="div-group-form">
-          <label>Contraseña</label>
+          <label className='label-form'>Contraseña</label>
           <input className='input-text' type="password" />
         </div>
         <div className="div-group-form">
-          <label>Seleccion Pais</label>
+          <label className='label-form'>Seleccion Pais</label>
           <select name="" id="">
             <option value="1">Opcion 1</option>
             <option value="2">Opcion 2</option>
@@ -23,11 +23,13 @@ const FormCssPuro = () => {
             <option value="4">Opcion 4</option>
           </select>
         </div>
-        {/* <div className="div-group-form">
-            <label>Tipo de tarjeta</label>
+        <div className="div-group-form">
+            <label className='label-form'>Tipo de tarjeta</label>
+            <div>
             <input type="radio" name="Debito" id="" />
             <input type="radio" name="Credito" id="" />
-        </div> */}
+            </div>
+        </div>
         <button className='btn-enviar'>Enviar</button>
       </form>
     </div>
