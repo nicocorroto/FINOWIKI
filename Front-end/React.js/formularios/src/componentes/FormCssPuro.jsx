8f788da8fdfb8000d4f8164c8d45a7c94cc7d661
IsaiasC1999 <100697426+IsaiasC1999@users.noreply.github.com>
2023-08-02 12:55:57 -0300

    creando los estados

--- Front-end/React.js/formularios/src/componentes/FormCssPuro.jsx
+++ Front-end/React.js/formularios/src/componentes/FormCssPuro.jsx
@@ -1,22 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../estilos/estilos-form.css'
 const FormCssPuro = () => {
+
+  const [nombre, setNombre] = useState('');
+  const [contraseña, setContraseña] = useState('');
+
+
+  function hangleSubmit(e) {
+    e.preventDefault();
+    console.log("hola");
+    const form = e.target;
+    const formData = new FormData(form);
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+  }
+
+
   return (
 
+
     <div className='conteiner-form'>
-        <h3>Formulario</h3>
-      <form className='form'>
+      <h3>Formulario</h3>
+      <form className='form' method="post" onSubmit={hangleSubmit}>
         <div className="div-group-form">
           <label className='label-form'>Nombre</label>
-          <input className='input-text' type="text" placeholder='Ingrese su nombre' />
+          <input className='input-text' value={nombre} name='nombre' onChange={e => setNombre(e.target.value)} 
+          type="text" placeholder='Ingrese su nombre' />
+          { nombre === '' ? <p>Esto no debe estar vacio</p> : <></> }
         </div>
         <div className="div-group-form">
           <label className='label-form'>Contraseña</label>
-          <input className='input-text' type="password" placeholder='Ingrese su contraseña' />
+          <input className='input-text' value={contraseña} name='contraseña' onChange={e => setContraseña(e.target.value)} type="password" placeholder='Ingrese su contraseña' />
         </div>
         <div className="div-group-form">
           <label className='label-form'>Seleccion Pais</label>
-          <select name="" id="">
+          <select name="DatosSelect" defaultValue='2' id="">
             <option value="1">Opcion 1</option>
             <option value="2">Opcion 2</option>
             <option value="3">Opcion 3</option>
@@ -24,22 +42,21 @@ const FormCssPuro = () => {
           </select>
         </div>
         <div className="div-group-form">
-            <label className='label-form'>Tipo de tarjeta</label>
-            <div className='conteiner-radio-inp'>
-            <input type="radio" name="Debito" id="" />
-            <label htmlFor="">Debito</label>
-            </div>
-            <div className='conteiner-radio-inp'>
-            <input type="radio" name="Credito" id="" />
-            <label htmlFor="">Credito</label>
-            </div>
-            
+          <label className='label-form'>Tipo de tarjeta</label>
+          <label>
+            <input type="radio" name="myRadio" value="debito" />
+            Option 1
+          </label>
+          <label>
+            <input type="radio" name="myRadio" value="credito" />
+            Option 2
+          </label>
+          
+
         </div>
         <div div className="div-group-form">
-            <label className='label-form'>Descripcion</label>
-            <textarea rows={4} >
-              Aqui puede ingresar una descripcion si es necesario
-            </textarea>
+          <label className='label-form'>Descripcion</label>
+          <textarea name='descripcion' rows={4} defaultValue="texto descriptivo text area " />
         </div>
         <button className='btn-enviar'>Enviar</button>
       </form>
